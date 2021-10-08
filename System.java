501bee7ff4dcb9dac8838d1576871be4756bad46
Karen Almeida <karenalmeida61@hotmail.com>
2021-10-08 12:12:11 -0300

    Criar métodos da Classe venda

--- System.java
+++ System.java
@@ -1,4 +1,5 @@
 package SystemSale;
 
 public class System {
+
 }
--- Venda.java
+++ Venda.java
@@ -6,4 +6,57 @@ import java.util.List;
 public class Venda {
     public List<Cliente>clienteList=new ArrayList<>();
     public List<Vendedor>vendedorList=new ArrayList<>();
+    private String DataDaCompra;
+    private double ValorDaCompra;
+
+    public Venda() {
+    }
+
+    public Venda(List<Cliente> clienteList, List<Vendedor> vendedorList, String dataDaCompra, double valorDaCompra) {
+        this.clienteList = clienteList;
+        this.vendedorList = vendedorList;
+        DataDaCompra = dataDaCompra;
+        ValorDaCompra = valorDaCompra;
+    }
+
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void setClienteList(List<Cliente> clienteList) {
+        this.clienteList = clienteList;
+    }
+
+    public List<Vendedor> getVendedorList() {
+        return vendedorList;
+    }
+
+    public void setVendedorList(List<Vendedor> vendedorList) {
+        this.vendedorList = vendedorList;
+    }
+
+    public String getDataDaCompra() {
+        return DataDaCompra;
+    }
+
+    public void setDataDaCompra(String dataDaCompra) {
+        DataDaCompra = dataDaCompra;
+    }
+
+    public double getValorDaCompra() {
+        return ValorDaCompra;
+    }
+
+    public void setValorDaCompra(double valorDaCompra) {
+        ValorDaCompra = valorDaCompra;
+    }
+
+    @Override
+    public String toString() {
+        return "\nVenda:" +
+                "\nClientes:" + clienteList +
+                "\nVendedores" + vendedorList +
+                "\nData da Compra" + DataDaCompra +
+                "\nValor da Compra" + ValorDaCompra;
+    }
 }
